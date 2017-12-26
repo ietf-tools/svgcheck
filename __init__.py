a8627bdf7c22c50021da48aafdb63b82ad8b5f8d
Jim Schaad <ietf@augustcellars.com>
2017-12-25 20:23:47 -0800

    and this file

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 
 # Static values
 __version__  = '0.0.1'
-NAME         = 'rfceditor'
+NAME         = 'RfcEditor'
 VERSION      = [ int(i) if i.isdigit() else i for i in __version__.split('.') ]
 
 
