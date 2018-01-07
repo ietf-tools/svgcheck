b879e5fb1d582003b0db49b95df7e630f22359e6
Jim Schaad <ietf@augustcellars.com>
2018-01-06 19:26:21 -0800

    Fix build problems
    * Correct slashes for Linux
    * Correct file name for pypy on windows

--- winbuild/appveyor_install_pypy.cmd
+++ winbuild/appveyor_install_pypy.cmd
@@ -1,3 +1,3 @@
-curl -fsSL -o pyp2.zip https://bitbucket.org/pypy/pypy/downloads/pypy2-v5.8.0-win32.zip
+curl -fsSL -o pypy2.zip https://bitbucket.org/pypy/pypy/downloads/pypy2-v5.8.0-win32.zip
 7z x pypy2.zip -oc:\
 c:\python34\scripts\virtualenv.exe -p c:\pypy2-v5.8.0-win32\pypy.exe c:\vp\pypy2
