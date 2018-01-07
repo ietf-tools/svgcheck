191d08e34e6023da6d452c555d02242e026c317e
Jim Schaad <ietf@augustcellars.com>
2018-01-06 20:03:54 -0800

    try again for pypy windows lxml

--- winbuild/appveyor_install_pypy.cmd
+++ winbuild/appveyor_install_pypy.cmd
@@ -2,3 +2,4 @@ curl -fsSL -o pypy2.zip https://bitbucket.org/pypy/pypy/downloads/pypy2-v5.8.0-w
 7z x pypy2.zip -oc:\
 c:\python34\scripts\virtualenv.exe -p c:\pypy2-v5.8.0-win32\pypy.exe c:\vp\pypy2
 pip install cython
+pip install -e git+https://github.com/lxml/lxml.git#egg=lxml
