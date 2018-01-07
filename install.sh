54c55e3678efdaf6dd76fdb5158cf7fd0c377b99
Jim Schaad <ietf@augustcellars.com>
2018-01-06 20:28:50 -0800

    Get MAC build to run

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    #  install some custom requirements on OSX
+    #  e.g. brew install pyenv-virtualenv
+
+    case "${TOXENV}" in
+	py27)
+	    # default version w/ the OS
+	;;
+
+	py36)
+	    brew install python3
+	    ;;
+
+    esac
+else
+    # Custom stuff for Linux
+fi
+
+    
