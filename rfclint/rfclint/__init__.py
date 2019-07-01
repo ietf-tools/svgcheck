1dccc816be7bc7e99a88c3a09956412a30d84107
Jim Schaad <ietf@augustcellars.com>
2019-07-01 14:29:19 -0400

    New releases which update the RNG file (#93)
    * New releases which update the RNG file
    
    Enough changes have been made to the V3 rng file that it really needs to get updated here as well.
    
    * Update SVG-1.2-RFC.rng
    
    Change capitialization of the name
    
    * Revert back include of svg
    
    The way that Henrik is doing this does not seem to work correctly for me
    
    * Update MANIFEST.in
    
    Need to get the new name here also

--- rfclint/rfclint/__init__.py
+++ rfclint/rfclint/__init__.py
@@ -1,6 +1,6 @@
 
 # Static values
-__version__  = '0.5.15'
+__version__  = '0.5.16'
 NAME         = 'rfclint'
 VERSION      = [ int(i) if i.isdigit() else i for i in __version__.split('.') ]
 
--- rfctools_common/rfctools_common/__init__.py
+++ rfctools_common/rfctools_common/__init__.py
@@ -1,4 +1,4 @@
 # Static values
-__version__  = '0.5.15'
+__version__  = '0.5.16'
 NAME         = 'rfctools_common'
 VERSION      = [ int(i) if i.isdigit() else i for i in __version__.split('.') ]
