ef9186bead06981145b76b925bd25637c68c7133
Jim Schaad <ietf@augustcellars.com>
2020-01-27 17:13:36 -0800

    Up the version number for rfclint

--- rfclint/rfclint/__init__.py
+++ rfclint/rfclint/__init__.py
@@ -3,7 +3,7 @@
 # ----------------------------------------------------
 
 # Static values
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 NAME = 'rfclint'
 VERSION = [int(i) if i.isdigit() else i for i in __version__.split('.')]
 
