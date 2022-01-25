5da8749f0505065634d1d1ada9921e86bda6464f
NGPixel <github@ngpixel.com>
2022-01-24 21:30:49 -0500

    chore: exclude non-svgcheck files

--- rfctools_common/README.rst
+++
@@ -1,6 +0,0 @@
-RFC Editor Tools Utility Code
-=============================
-
-This module contains a set of utility functions used by some of the RFC Editor tools.
-Long term, the goal is to incorperate these functions back into xml2rfc but having it
-separate allows for an easier modification of the functions as needed.
--- rfctools_common/rfctools_common/__init__.py
+++
@@ -1,8 +0,0 @@
-# --------------------------------------------------
-# Copyright The IETF Trust 2011-2019, All Rights Reserved
-# --------------------------------------------------
-
-# Static values
-__version__  = '0.6.1'
-NAME         = 'rfctools_common'
-VERSION      = [ int(i) if i.isdigit() else i for i in __version__.split('.') ]
