04dbd28ef6825951023a5d17d8b3c1436acc04d9
Nicolas Giard <github@ngpixel.com>
2022-01-27 14:26:14 -0500

    ci: fix setup.py to work with github actions

--- setup.py
+++ setup.py
@@ -11,74 +11,18 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as file:
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as file:
     long_description = file.read()
-    long_description = long_description.replace('\r', '')
-
-# Get the requirements from the local requirements.txt file
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as file:
-    requirements = file.read().splitlines()
-
-# Get additional items from the local MANIFEST.in file
-with open(path.join(here, 'MANIFEST.in'), encoding='utf-8') as file:
-    extra_files = [ l.split()[1] for l in file.read().splitlines() if l ]
-
-def parse(changelog):
-    ver_line = "^([a-z0-9+-]+) \(([^)]+)\)(.*?) *$"
-    sig_line = "^ ?-- ([^<]+) <([^>]+)>  (.*?) *$"
-
-    entries = []
-    if type(changelog) == type(''):
-        changelog = open(changelog, mode='rU', encoding='utf-8')
-    for line in changelog:
-        if re.match(ver_line, line):
-            package, version, rest = re.match(ver_line, line).groups()
-            entry = {}
-            entry["package"] = package
-            entry["version"] = version
-            entry["logentry"] = ""
-        elif re.match(sig_line, line):
-            author, email, date = re.match(sig_line, line).groups()
-            entry["author"] = author
-            entry["email"] = email
-            entry["datetime"] = date
-            entry["date"] = " ".join(date.split()[:3])
-
-            entries += [ entry ]
-        else:
-            entry["logentry"] += line.rstrip() + '\n'
-    changelog.close()
-    return entries
-
-changelog_entry_template = """
-Version %(version)s (%(date)s)
-------------------------------------------------
-
-%(logentry)s
-
-"""
-
-long_description += """
-Changelog
-=========
-
-""" + "\n".join([ changelog_entry_template % entry for entry in parse("changelog")[:3] ])
-
-long_description = long_description.replace('\r', '')
-
-import svgcheck
 
 setup(
     name='svgcheck',
 
-    # Versions should comply with PEP440.
-    version=svgcheck.__version__,
-
     description="Verify that an svg file is compliant with the RFC standards.",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     
     # The projects main homepage.
-    url='https://github.com/ietf-tools/RfcEditor/tree/master/svgcheck',
+    url='https://github.com/ietf-tools/svgcheck/',
 
     # Author details
     author='Jim Schaad',
@@ -94,9 +38,13 @@ setup(
         'Topic :: Text Processing',
         'Topic :: Text Processing :: Markup :: XML',
         'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         ],
 
     # What does your project relate to?
