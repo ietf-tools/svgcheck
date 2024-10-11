25f2f0db563fc1551c0c3433428a24dc81935f4f
Kesara Rathnayake <kesara@fq.nz>
2024-10-11 14:00:05 +1300

    fix: Create temp file and delete after processing

--- svgcheck/run.py
+++ svgcheck/run.py
@@ -108,25 +108,17 @@ def main():
         wp.color_threshold = options.grey_level
 
     if len(args) < 1:
-        # delete_on_close was introduced in Python 3.12
-        have_delete_on_close = sys.version_info >= (3, 12)
-
-        if sys.platform.lower().startswith("win") and not have_delete_on_close:
-            # On Windows with Python < 3.12, can not reopen closed temp files.
-            sys.exit("Stdin is only supported on Windows with Python 3.12 and above.")
-
-        delete_on_close = {"delete_on_close": False} if have_delete_on_close else {}
-
-        with tempfile.NamedTemporaryFile(mode="w+b", **delete_on_close) as tmp_file:
-            data = sys.stdin.buffer.read()
-            tmp_file.write(data)
-            if have_delete_on_close:
+        source = None
+        try:
+            with tempfile.NamedTemporaryFile(mode="w+b", delete=False) as tmp_file:
+                data = sys.stdin.buffer.read()
+                tmp_file.write(data)
                 tmp_file.close()
-            else:
-                # Prior to Python 3.12 file is deleted as soon as it's closed
-                tmp_file.seek(0)
-            source = tmp_file.name
-            process_svg(options, source)
+                source = tmp_file.name
+                process_svg(options, source)
+        finally:
+            if os.path.exists(source):
+                os.remove(source)
     else:
         source = args[0]
         if not os.path.exists(source):
